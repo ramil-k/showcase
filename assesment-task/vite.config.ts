daabcdfbdd7fb3b4b662ea1ca2f4e88901ea4625
Ramil Karimov <rkarimov@avaya.com>
2022-11-23 13:06:56 +0500

    Added build configuration to work on the github pages

--- assesment-task/vite.config.ts
+++ assesment-task/vite.config.ts
@@ -4,6 +4,8 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
+  build: { outDir: "../docs" },
   plugins: [
     react(),
     svgr({
